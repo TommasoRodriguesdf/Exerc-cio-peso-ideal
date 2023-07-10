c781cda3d85cfe03653b7c5a82c189c231233d80
TommasoRodriguesdf <131806144+TommasoRodriguesdf@users.noreply.github.com>
2023-07-10 16:04:35 -0400

    Update exercício.js

--- assets/exercício.js
+++ assets/exercício.js
@@ -1,28 +1,37 @@
-//Exercício1
+ function calculaPeso(peso, altura, sereno){
+  if (sereno === 'F' ){
+    feminino = 62.1 * altura -44.7 ;
+    console.log(feminino);
+    
+    }else if (sereno === 'M' ){
+    masculino = 72.7 * altura - 58 ;
+    console.log(masculino);
+    
+    }else{
+    console.log('! algo esta incorrendo tende de novo !' );
+    
+    }
+    return peso;
 
-let peso; 
-let sereno;
-let altura; 
-peso = prompt('Digite seu peso para descobrir seu peso ideal ?');
-altura = prompt('Digite seu altura para descobrir seu peso ideal ?');
-sereno = prompt('Digite F para feminino ou M para masculino ?');
+ } 
 
-sereno = sereno.toUpperCaser();
-
-if (sereno === 'F' ){
-feminino = 62.1 * altura -44.7 ;
-console.log(feminino);
-
-}else if (sereno === 'M' ){
-masculino = 72.7 * altura - 58 ;
-console.log(masculino);
-
-}else{
-console.log('! algo esta incorrendo tende de novo !' );
-
+function imprimePesoIdeal (peso) {
+  alert('Peso ideal:' + peso. toFixed(2));
 }
 
-const pessoa ={
+function main(){
+  let peso = parseFloat(prompt( 'Digite seu peso para descobrir seu peso ideal ?')); 
+  let altura = parseFloat(prompt('Digite seu altura para descobrir seu peso ideal ?')); 
+  let sereno = prompt('Digite F para feminino ou M para masculino ?');
+  
+  peso = calculaPeso(peso, altura, sereno.toUpperCase() )
+  imprimePesoIdeal(peso);
+}
+
+main();
+
+
+  const pessoa ={
     nome:'Lucas Paquetá',
     idade:27,
     peso:53,
@@ -35,14 +44,3 @@ const pessoa ={
    for(let artributo in pessoa){
      console.log(artributo + " : " + pessoa[artributo]);
    }
-
-
-
-
-
-
-
-
-
-
-
